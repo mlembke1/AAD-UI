db853a04c387a5ea90c2fd9d254e9e2d428b97a6
Mitch Lembke <mitchell.lembke@gmail.com>
2018-09-12 14:02:52 -0400

    Landing page layout complete. Needs router/navigation functionality.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Header from '../Header/Header'
 import PageFooter from '../PageFooter/PageFooter'
+import { Button } from 'react-materialize'
 
 
 class LandingPage extends Component {
@@ -13,7 +14,22 @@ class LandingPage extends Component {
       <body>
         <Header />
           <main>
-            <div>Welcome to AAD! </div>
+            <div id="aad-title-container">
+              <div className="aad-title">ADVANCED</div>
+              <div className="aad-title">ANALYTIC</div>
+              <div className="aad-title">DEMONSTRATOR</div>
+            </div>
+
+            <div id="aad-paragraph">
+              <h5>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.
+              </h5>
+            </div>
+
+            <div id="signup-login-buttons-container">
+              <Button className="signup-login-buttons" waves='light'>Signup</Button>
+              <Button className="signup-login-buttons" waves='light'>Login</Button> 
+            </div>
           </main> 
         <PageFooter />
        </body> 
