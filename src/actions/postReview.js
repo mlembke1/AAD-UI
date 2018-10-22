60e8acedfa0c0732ad49a3b0fb67b315b6adf2b8
Mitch Lembke <mitchell.lembke@gmail.com>
2018-10-22 11:52:29 -0400

    Added error handling for CRUD operations in the case of no internet connection.

--- src/actions/postReview.js
+++ src/actions/postReview.js
@@ -19,13 +19,21 @@ export const postReview = object => dispatch => {
             console.log('POST_REVIEW RESPONSE ', json)
             if(json.status === 200) {
                 window.Materialize.toast('Post Successful!', 1300)
+                var element = document.getElementById("toast-container");
+                element.classList.add("success");
                 return dispatch({ type:'POST_REVIEW_SUCCESS' })
             } else {
+                window.Materialize.toast('Post Failed. Please Check Your Internet Connection and/or post your problem in the Slack Channel', 3300)
+                var element = document.getElementById("toast-container");
+                element.classList.add("failure");
                 return dispatch({ type:'POST_REVIEW_FAILED' })
             }
         })
         .catch(err => {
             console.log('ERROR WITH POST_REVIEW REQUEST', err)
+            window.Materialize.toast('Post Failed. Please Check Your Internet Connection and/or post your problem in the AAD-Users Slack Channel.', 10000)
+            var element = document.getElementById("toast-container");
+            element.classList.add("failure");
             return dispatch({ type:'POST_REVIEW_FAILED' })
         }) 
   }
--- src/actions/removeFile.js
+++ src/actions/removeFile.js
@@ -13,10 +13,15 @@ export const removeFile = reviewId => dispatch => {
     .then(result => result.json())
     .then(payload => {
       console.log('REMOVE FILE JSON RESPONSE', payload)
-        window.Materialize.toast('File Removed Successful!', 1300)
+      window.Materialize.toast('Remove File Successful!', 1300)
+      var element = document.getElementById("toast-container");
+      element.classList.add("success");
       return dispatch({ type:'REMOVE_FILE_SUCCESS', payload: reviewId })
     })
     .catch(err => {
+        window.Materialize.toast('Remove File Failed. Please Check Your Internet Connection and/or post your problem in the AAD-Users Slack Channel.', 10000)
+      var element = document.getElementById("toast-container");
+      element.classList.add("failure");
         return dispatch({ type: 'REMOVE_FILE_FAILED'})
     })
   }
